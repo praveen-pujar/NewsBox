683d1063dd0e8a397a6c48d6e148d423f219ef9a
praveen-pujar <pujarpraveen9@gmail.com>
2021-01-19 10:55:36 +0530

    updated service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,24 +5,78 @@ const assets = [
     "/home.html",
     "/src/index.js",
     "/src/styles.css",
+    "offline.html",
     "/composer.json",
     "/src/img.jpg",
     "/src/news.jpg",
 ]
 
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-      caches.open(newsapp).then(cache => {
-        cache.addAll(assets)
-      })
-    )
-  })
+// self.addEventListener("install", installEvent => {
+//     installEvent.waitUntil(
+//       caches.open(newsapp).then(cache => {
+//         cache.addAll(assets)
+//       })
+//     )
+//   })
 
 
-self.addEventListener("fetch", fetchEvent => {
-  fetch.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
+// self.addEventListener("fetch", fetchEvent => {
+//   fetch.respondWith(
+//     caches.match(fetchEvent.request).then(res => {
+//       return res || fetch(fetchEvent.request)
+//     })
+//   )
+// })
+
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function(){
+  console.log("Installing web app");
+  return caches.open("offline").then(function(cache) {
+    console.log("caching index and important routes");
+    return cache.addAll(assets);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log(response.url + " was cached");
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+     if(!matching || matching.status == 404) {
+       return cache.match("offline.html");
+     } else {
+       return matching;
+     }
+    });
+  });
+};
