0dec145441f0173b061b1b450df9d45533988019
praveen-pujar <pujarpraveen9@gmail.com>
2021-01-19 11:04:46 +0530

    updated service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 if(navigator.onLine){
 
     if (!navigator.serviceWorker.controller) {
-        navigator.serviceWorker.register("/sw.js").then(function(reg) {
+        navigator.serviceWorker.register("/serviceWorker.js").then(function(reg) {
             console.log("Service worker has been registered for scope: " + reg.scope);
         });
     }
